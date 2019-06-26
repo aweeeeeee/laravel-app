04665df256b2d770ce1e56dfc633bf55191db9d2
Norman Bicomong <normanb@SourcefitMNL.com>
2019-06-26 15:16:01 +0800

    created middleware

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BotController extends Controller
+{
+    public function bot(Request $request) {
+        
+    }
+    
+    private function sendTextMessage($recipientId, $messageText) {
+        
+    }
+}
